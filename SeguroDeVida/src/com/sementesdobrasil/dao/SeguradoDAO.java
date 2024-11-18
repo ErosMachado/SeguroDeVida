a424e0c2be95edde0d5299d9fc168ab282e85f31
ErosMachado <erosrodriguesmachado@gmail.com>
2024-11-18 13:14:15 -0300

    classe seguradoDAO --> novas modificações

--- SeguroDeVida/src/com/sementesdobrasil/dao/SeguradoDAO.java
+++ SeguroDeVida/src/com/sementesdobrasil/dao/SeguradoDAO.java
@@ -1,5 +1,25 @@
 package com.sementesdobrasil.dao;
 
-public class SeguradoDAO {
+import com.sementesdobrasil.model.Segurado;
+import com.sementesdobrasil.util.DatabaseConnection;
+import java.sql.*;
 
+public class SeguradoDAO {
+	DatabaseConnection conexao = new DatabaseConnection();
+	private Connection connection = conexao.conectar();
+
+	public void save(Segurado segurado) throws SQLException {
+		String sql = "INSERT INTO segurados (nome, data_nascimento, genero, email, telefone, cpf, cep) VALUES (?, ?, ?, ?, ?, ?, ?)";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, segurado.getNome());
+		stmt.setDate(2, Date.valueOf(segurado.getDataNascimento()));
+		stmt.setString(3, segurado.getGenero());
+		stmt.setString(4, segurado.getEmail());
+		stmt.setString(5, segurado.getTelefone());
+		stmt.setString(6, segurado.getCpf());
+		stmt.setString(7, segurado.getCep());
+		stmt.executeUpdate();
+		stmt.close();
+	}
+	// Métodos adicionais: buscar, atualizar, deletar segurados
 }
--- SeguroDeVida/src/com/sementesdobrasil/main/Application.java
+++ SeguroDeVida/src/com/sementesdobrasil/main/Application.java
@@ -1,17 +1,19 @@
 package com.sementesdobrasil.main;
+
 import com.sementesdobrasil.view.CadastroSeguradoView;
 import com.sementesdobrasil.service.SeguradoService;
 import com.sementesdobrasil.controller.CadastroSeguradoController;
 import javax.swing.SwingUtilities;
+
 public class Application {
- public static void main(String[] args) {
- SwingUtilities.invokeLater(new Runnable() {
- public void run() {
- CadastroSeguradoView view = new CadastroSeguradoView();
- SeguradoService service = new SeguradoService();
- new CadastroSeguradoController(view, service);
- view.setVisible(true);
- }
- });
- }
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				CadastroSeguradoView view = new CadastroSeguradoView();
+				SeguradoService service = new SeguradoService();
+				new CadastroSeguradoController(view, service);
+				view.setVisible(true);
+			}
+		});
+	}
 }
