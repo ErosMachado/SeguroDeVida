53f0eee5b2bef7cc0a831b70ae598566b16966f9
PedroHenrique1606-ctrl <luckiniko0@gmail.com>
2024-11-19 16:05:58 -0300

    Login

---
+++ SeguroDeVida/src/com/sementesdobrasil/view/LoginBuilder.java
@@ -0,0 +1,43 @@
+package com.sementesdobrasil.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class LoginBuilder {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginBuilder window = new LoginBuilder();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LoginBuilder() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 513, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
